4a8074b3cdd0e5de88d350bdf7e393bdd58a2171
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-22 14:13:31 -0600

    Finish book class

---
+++ lib/book.rb
@@ -0,0 +1,26 @@
+class Book
+
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+
+
+  def initialize(data)
+    @author_first_name = data[:author_first_name]
+    @author_last_name  = data[:author_last_name]
+    @title             = data[:title]
+    @publication_date  = data[:publication_date]
+  end
+
+  def author
+    @author_first_name + " " + @author_last_name
+  end
+
+  def publication_year
+    # binding.pry
+    publication_date_arr = @publication_date.split(",")
+    publication_year_info = publication_date_arr[1].strip
+  end
+
+end
